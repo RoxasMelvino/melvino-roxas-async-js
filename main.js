541e65271b8098372e46ffeecd9838a4fc0f5656
RoxasMelvino <melvinoroxasbvt2024@gmail.com>
2024-10-09 19:08:33 -0700

    finish part 5

--- main.js
+++ main.js
@@ -13,4 +13,30 @@ const alice1 = document.querySelector("#alice1");
 const alice2 = document.querySelector("#alice2");
 const alice3 = document.querySelector("#alice3");
 
-  
+
+
+// "callback hell" promise version
+// alice1.animate(aliceTumbling, aliceTiming).finished.then(() => {
+//   alice2.animate(aliceTumbling, aliceTiming).finished.then(() => {
+//     alice3.animate(aliceTumbling, aliceTiming)
+//   })
+// })
+
+// promise chain version
+// alice1.animate(aliceTumbling, aliceTiming).finished
+// .then(() => alice2.animate(aliceTumbling, aliceTiming).finished)
+// .then(() => alice3.animate(aliceTumbling, aliceTiming))
+
+// async & await version
+
+async function animationSequence() {
+  try {
+    await alice1.animate(aliceTumbling, aliceTiming).finished
+    await alice2.animate(aliceTumbling, aliceTiming).finished
+    await alice3.animate(aliceTumbling, aliceTiming).finished
+  } catch (error) {
+    console.error(error)
+  }
+}
+
+animationSequence();
